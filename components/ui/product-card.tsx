9d5cb528e71c66c0177572141c02820dff2c6ac1
alexandru_beep <ckerarahpikc@gmail.com>
2025-06-01 01:26:00 +0300

    Add to cart functionality (Store)
    new file:   app/(routes)/cart/components/cart-item.tsx
      1. this holds the data of the products from the cart (currency, removing button, name, size, color)
    
    new file:   app/(routes)/cart/components/summary.tsx
      1. this holds the order summary, like total of all the products in the cart
      2. will also use a checkout button to book all this stuff
    
    new file:   app/(routes)/cart/page.tsx
      1. this file contains all the items in the cart with their respective summary
      2. I also used a mounted trick
    
    modified:   app/layout.tsx
      1. added ToastProvider to be able to use toast inside {children}
    
    modified:   components/navbar-actions.tsx
      1. on clicking button we render the /cart page which holds the products
      2. also the items are now flexible and holds the actual count of the products
    
    modified:   components/ui/product-card.tsx
      1. I add the onAddToCart functionality which will add to cart on click (obviously)
    
    new file:   hooks/use-cart.ts
      1. store the list of the products in the cart (items)
      2. allows to add new product to the cart (addItem), so that we could not add any duplicates
      3. allows to delete the product from the cart with id (removeItem)
      4. allows to clear entire cart (removeAll)
      5. saves the state of the cart to localStorage with middleware (persist)
      6. shows the push notification on adding and deleting products
    
    new file:   providers/toast-provider.tsx
      1. holds the toast provider (nothing special)

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -9,6 +9,7 @@ import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
 import usePreviewModal from "@/hooks/use-preview-modal";
 import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
   data: Product;
@@ -17,6 +18,7 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const previewModal = usePreviewModal();
   const router = useRouter();
+  const cart = useCart();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
@@ -30,6 +32,8 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
 
   const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
     event.stopPropagation();
+
+    cart.addItem(data);
   };
 
   return (
