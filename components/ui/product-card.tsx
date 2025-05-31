1e3ea77de81e0c6cff01531a2adb9bf8a7eaa5d0
alexandru_beep <ckerarahpikc@gmail.com>
2025-06-01 00:54:32 +0300

    Product preview modal component (Store)
    modified:   app/layout.tsx
      1. now will use ModalProvider which lets us to use the actual modal inside chidlren
    
    modified:   components/info.tsx
      1. updated the button 'add to cart'
    
    new file:   components/preview-modal.tsx
      1. this file covers the modal component preview itself which holds the actual data that user will see by clickin on preview button
    
    new file:   components/ui/modal.tsx
      1. new ui component of the modal
    
    modified:   components/ui/product-card.tsx
      1. expand button is now working and shows the modal
      2. added new function 'onPreview' it's not working for now
      3. updated the Image to minimize the LCP from 4s to 9s (nice)
    
    new file:   hooks/use-preview-modal.ts
      1. this file holds and manages the state of the modal window
      2. is used to show the info of the product in the modal window on clicking 'expand' (review)
    
    new file:   providers/modal-provider.tsx
      1. this file prevent renders of the modal on the server, to avoid errors about client libraries and state

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Image from "next/image";
+import { MouseEventHandler } from "react";
 import { Expand, ShoppingCart } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 import Currency from "@/components/ui/currency";
 import IconButton from "@/components/ui/icon-button";
+import usePreviewModal from "@/hooks/use-preview-modal";
 import { Product } from "@/types";
 
 interface ProductCard {
@@ -13,10 +15,21 @@ interface ProductCard {
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  };
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
   };
 
   return (
@@ -34,8 +47,12 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton icon={<Expand size={20} className="text-gray-600" />} />
             <IconButton
+              onClick={onPreview}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
